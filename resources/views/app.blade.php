253a8f153cdea5bb84731e500a39c1b0b3a72cf6
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-06-14 08:50:25 +0700

    Install daisy ui tailwind via cdn

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,11 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+    <!-- tailwind daisyUI -->
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.15.3/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
